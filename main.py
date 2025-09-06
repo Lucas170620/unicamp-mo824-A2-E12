cec64208827acba9618be3e4d58a6b43c72243da
Lucas Candido <69920451+Lucas170620@users.noreply.github.com>
2025-09-06 17:22:30 -0300

    var alfa

--- main.py
+++ main.py
@@ -4,40 +4,57 @@ from src.problems.qbf.solvers.grasp_qbf import GRASP_QBF, GRASP_QBF_First_Improv
 
 
 if __name__ == "__main__":
-    n = 20
-    instancia = f"instances/qbf/qbf0{n}"
-    interations = n
+    n = 400
+    instancia = f"instances/qbf/qbf{n}"
+    interations = 100
+    alpha_1 = 0.1
+    alpha_2 = 0.9
+    resultados = [["name", "maxVal", "time"]]
     start_time = time.time()
-    grasp = GRASP_QBF(alpha=0.3, iterations=interations, filename=instancia)
+    grasp = GRASP_QBF(alpha=alpha_2, iterations=interations, filename=instancia)
     best_sol = grasp.solve()
     cost = - best_sol.cost
-    print(f"maxVal Padrao = {cost}")
     end_time = time.time()
     total_time = end_time - start_time
-    print(f"Time Padrao = {total_time} seg")
+    resultados.append(["GRASP QBF PADRÃO Best Improvement alfa=0.3", cost, total_time])
+
+    start_time = time.time()
+    grasp = GRASP_QBF(alpha=alpha_1, iterations=interations, filename=instancia)
+    best_sol = grasp.solve()
+    cost = - best_sol.cost
+    resultados.append(["GRASP QBF PADRÃO Best Improvement alfa=0.1", cost, total_time])
+    end_time = time.time()
 
     reactive_grasp = ReactiveGRASP_QBF(alpha_values=[0.1, 0.3, 0.5, 0.7, 0.9], iterations=interations, filename=instancia)
     best_sol_reactive = reactive_grasp.solve()
     cost_reactive = - best_sol_reactive.cost
-    print(f"maxVal (Reactive) = {cost_reactive}")
     end_time_reactive = time.time()
     total_time_reactive = end_time_reactive - start_time
-    print(f"Time (Reactive) = {total_time_reactive} seg")
+    resultados.append(["Reactive GRASP QBF Best Improvement", cost_reactive, total_time_reactive])
 
     start_time = time.time()
-    grasp_first_improvement = GRASP_QBF_First_Improvement(alpha=0.3, iterations=interations, filename=instancia)
+    grasp_first_improvement = GRASP_QBF_First_Improvement(alpha=alpha_2, iterations=interations, filename=instancia)
     best_sol_first_improvement = grasp_first_improvement.solve()
     cost_first_improvement = - best_sol_first_improvement.cost
-    print(f"maxVal Padrao + (First Improvement) = {cost_first_improvement}")
     end_time_first_improvement = time.time()
     total_time_first_improvement = end_time_first_improvement - start_time
-    print(f"Time Padrao + (First Improvement) = {total_time_first_improvement} seg")
+    resultados.append(["GRASP QBF First Improvement alfa=0.3", cost_first_improvement, total_time_first_improvement])
+
+    start_time = time.time()
+    grasp_first_improvement = GRASP_QBF_First_Improvement(alpha=alpha_1, iterations=interations, filename=instancia)
+    best_sol_first_improvement = grasp_first_improvement.solve()
+    cost_first_improvement = - best_sol_first_improvement.cost
+    end_time_first_improvement = time.time()
+    total_time_first_improvement = end_time_first_improvement - start_time
+    resultados.append(["GRASP QBF First Improvement alfa=0.1", cost_first_improvement, total_time_first_improvement])
 
     start_time = time.time()
     reactive_grasp_first_improvement = ReactiveGRASP_QFG_First_Improvement(alpha_values=[0.1, 0.3, 0.5, 0.7, 0.9], iterations=interations, filename=instancia)
     best_sol_reactive_first_improvement = reactive_grasp_first_improvement.solve()
     cost_reactive_first_improvement = - best_sol_reactive_first_improvement.cost
-    print(f"maxVal (Reactive First Improvement) = {cost_reactive_first_improvement}")
     end_time_reactive_first_improvement = time.time()
     total_time_reactive_first_improvement = end_time_reactive_first_improvement - start_time
-    print(f"Time (Reactive First Improvement) = {total_time_reactive_first_improvement} seg")
+    resultados.append(["Reactive GRASP QBF First Improvement", cost_reactive_first_improvement, total_time_reactive_first_improvement])
+
+    for resultado in resultados:
+        print(f"{resultado[0]}: {resultado[1]} em {resultado[2]} segundos")
